56f1ade7069bd12e5033ff2282b16f3cc4a7c4f3
lincoln <lincolnskyrim_99@hotmail.com>
2026-07-23 16:14:43 -0300

    feat: event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -2,3 +2,24 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
+
+app.post("/events", (req, res) => {
+  console.log("Received Event:", req.body.type);
+
+  axios.post("http://localhost:3000/events", req.body).catch((err) => {
+    console.log("Error sending event to posts service:", err.message);
+  });
+
+  axios.post("http://localhost:3001/events", req.body).catch((err) => {
+    console.log("Error sending event to comments service:", err.message);
+  });
+
+  axios.post("http://localhost:3002/events", req.body).catch((err) => {
+    console.log("Error sending event to query service:", err.message);
+  });
+  res.send({ status: "OK" });
+});
+
+app.listen(3003, () => {
+  console.log("Listening on 3003");
+});
